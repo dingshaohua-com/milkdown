bf5e734f1c639e284fdbd3c715e1ec43cd7563b6
丁少华 <960423114@qq.com>
2025-05-20 00:22:37 +0800

    fix: 使用了最新的补丁

--- packages/milkdown-editor/src/index.tsx
+++ packages/milkdown-editor/src/index.tsx
@@ -2,14 +2,21 @@ import { Crepe } from '@milkdown/crepe';
 import '@milkdown/crepe/theme/frame.css'; // 一个完整主题（可选，其它可选项见下）
 import '@milkdown/crepe/theme/common/style.css'; // 基础样式（必需）
 import { Milkdown, MilkdownProvider, useEditor } from '@milkdown/react';
+// import { listener } from '@milkdown/kit/plugin/listener'
+
+
 
 const CrepeEditor: React.FC = () => {
   const { get } = useEditor((root) => {
     const crepe = new Crepe({ root, defaultValue: 'Hello, Milkdown!' });
+    // crepe.editor.use(listener)
     crepe.on((listener) => {
-      listener.updated((ctx, doc, prevDoc) => {
-        console.log(123);
+      console.log(listener);
+      
+      listener.selectionUpdated((ctx, selection, prevSelection) => {
+        console.log(selection, prevSelection);
       });
+    
     });
     return crepe;
   });
