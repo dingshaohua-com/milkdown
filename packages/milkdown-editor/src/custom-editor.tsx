f36d1847bd1a72e4df32b158fe0915114daf88d0
丁少华 <960423114@qq.com>
2025-06-03 20:04:52 +0800

    fix

--- packages/milkdown-editor/src/custom-editor.tsx
+++ packages/milkdown-editor/src/custom-editor.tsx
@@ -2,21 +2,26 @@ import './style.scss';
 import MenuBar from './menu-bar';
 import MdEditor from './md-editor';
 import emitter from './utils/emitter';
-import { Crepe } from '@milkdown/crepe';
 import { EditorConfig } from '../global';
-import '@milkdown/crepe/theme/frame.css'; // 一个完整主题（可选，其它可选项见下）
+// import '@milkdown/crepe/theme/frame.css'; // 一个完整主题（可选，其它可选项见下）
 import { useState, useEffect } from 'react';
+import { nord } from '@milkdown/theme-nord';
 import { useBlock } from './plugin/block-view';
-import '@milkdown/crepe/theme/common/style.css'; // 基础样式（必需）
+// import '@milkdown/crepe/theme/common/style.css'; // 基础样式（必需）
 import { replaceAll } from '@milkdown/kit/utils';
 import { editorViewCtx } from '@milkdown/kit/core';
 import { useSlashPlus } from './plugin/slash-menu-plus';
 import { useSlashBlock } from './plugin/slash-menu-block';
+import { commonmark } from '@milkdown/kit/preset/commonmark';
+import { usePluginViewFactory } from '@prosemirror-adapter/react';
+import { install as mblockViewInstall } from './plugin/mblock-view';
+import { Editor, rootCtx, defaultValueCtx } from '@milkdown/kit/core';
 import { Milkdown, MilkdownProvider, useEditor } from '@milkdown/react';
 import { ProsemirrorAdapterProvider } from '@prosemirror-adapter/react';
 import { EditorConfigProvider, useEditorDefaultConfig } from './config-ctx';
 
 const CrepeEditor: React.FC<EditorConfig> = (props) => {
+  const pluginViewFactory = usePluginViewFactory();
   const block = useBlock();
   const slashBlock = useSlashBlock();
   const slashPlus = useSlashPlus();
@@ -27,49 +32,67 @@ const CrepeEditor: React.FC<EditorConfig> = (props) => {
   const [isMdEditorFocused, setIsMdEditorFocused] = useState(false);
   const [content, setContent] = useState(config.content || 'Hello, Milkdown!');
 
-  const { get } = useEditor((root) => {
-    const crepe = new Crepe({
-      root,
-      defaultValue: content,
-      features: {
-        'block-edit': false,
-      },
-    });
-    crepe.on((listener) => {
-      listener.selectionUpdated((ctx) => {
-        const editorViewCtxVal = ctx.get(editorViewCtx);
-        if (editorViewCtxVal.hasFocus && editorViewCtxVal.hasFocus()) {
-          emitter.emit('selectionUpdated' as never);
-        }
-      });
-      listener.markdownUpdated((ctx, markdown) => {
-        const isFocused = ctx.get(editorViewCtx).hasFocus();
-        if (isFocused) {
-          setContent(markdown);
-          config.onChange && config.onChange(markdown);
-        }
-      });
-    });
-    crepe.editor
-      .config((ctx: any) => {
-        slashBlock.config(ctx);
-      })
-      .use(slashBlock.plugin);
+  // const { get } = useEditor((root) => {
+  //   const crepe = new Crepe({
+  //     root,
+  //     defaultValue: content,
+  //     features: {
+  //       'block-edit': false,
+  //     },
+  //   });
+  //   crepe.on((listener) => {
+  //     listener.selectionUpdated((ctx) => {
+  //       const editorViewCtxVal = ctx.get(editorViewCtx);
+  //       if (editorViewCtxVal.hasFocus && editorViewCtxVal.hasFocus()) {
+  //         emitter.emit('selectionUpdated' as never);
+  //       }
+  //     });
+  //     listener.markdownUpdated((ctx, markdown) => {
+  //       const isFocused = ctx.get(editorViewCtx).hasFocus();
+  //       if (isFocused) {
+  //         setContent(markdown);
+  //         config.onChange && config.onChange(markdown);
+  //       }
+  //     });
+  //   });
+  //   crepe.editor
+  //     .config((ctx: any) => {
+  //       slashBlock.config(ctx);
+  //     })
+  //     .use(slashBlock.plugin);
 
-    crepe.editor
-      .config((ctx: any) => {
-        slashPlus.config(ctx);
-      })
-      .use(slashPlus.plugin);
+  //   crepe.editor
+  //     .config((ctx: any) => {
+  //       slashPlus.config(ctx);
+  //     })
+  //     .use(slashPlus.plugin);
 
-    crepe.editor
-      .config((ctx: any) => {
-        block.config(ctx);
-      })
-      .use(block.plugin);
+  //   crepe.editor
+  //     .config((ctx: any) => {
+  //       block.config(ctx);
+  //     })
+  //     .use(block.plugin);
 
-    return crepe;
-  });
+  //   return crepe;
+  // });
+
+  const { get, loading } = useEditor(
+    (root) => {
+      const editor = Editor.make()
+        .config(nord)
+        .config((ctx) => {
+          ctx.set(rootCtx, root);
+          ctx.set(defaultValueCtx, content);
+        })
+        .use(commonmark);
+      mblockViewInstall(editor, pluginViewFactory);
+      return editor;
+    },
+
+    // .then(() => {
+    //   console.log("Editor created");
+    // })
+  );
 
   const editor = get();
 
--- packages/milkdown-editor/src/utils/editor-helper.ts
+++ packages/milkdown-editor/src/utils/editor-helper.ts
@@ -7,11 +7,11 @@ export const checkMarkActive = (type: string, editor: Editor) => {
   const typeObject = editorState.schema.marks[type];
   const { from, to, empty } = editorState.selection;
   let isActive;
-  if (empty) {
-    isActive = typeObject.isInSet(editorState.storedMarks || editorState.selection.$from.marks()) != null;
-  } else {
-    isActive = editorState.doc.rangeHasMark(from, to, typeObject);
-  }
+  // if (empty) {
+  //   isActive = typeObject.isInSet(editorState.storedMarks || editorState.selection.$from.marks()) != null;
+  // } else {
+  //   isActive = editorState.doc.rangeHasMark(from, to, typeObject);
+  // }
   return isActive;
 };
 
