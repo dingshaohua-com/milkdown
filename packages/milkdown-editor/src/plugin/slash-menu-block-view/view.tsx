52e94a810c6ed9f305ec469086fbb57efa41133a
丁少华 <960423114@qq.com>
2025-06-04 13:38:42 +0800

    fix

--- packages/milkdown-editor/src/plugin/slash-menu-block-view/view.tsx
+++ packages/milkdown-editor/src/plugin/slash-menu-block-view/view.tsx
@@ -4,10 +4,12 @@ import { Ctx } from '@milkdown/kit/ctx';
 import { useInstance } from '@milkdown/react';
 import { callCommand } from '@milkdown/kit/utils';
 import { editorViewCtx } from '@milkdown/kit/core';
+import { codeImg, boldImg } from '../../utils/img-helper';
+import { TextSelection } from '@milkdown/kit/prose/state';
 import { SlashProvider } from '@milkdown/kit/plugin/slash';
 import React, { useCallback, useEffect, useRef } from 'react';
 import { usePluginViewContext } from '@prosemirror-adapter/react';
-import { createCodeBlockCommand } from '@milkdown/kit/preset/commonmark';
+import { createCodeBlockCommand, toggleStrongCommand, toggleEmphasisCommand } from '@milkdown/kit/preset/commonmark';
 
 const View = () => {
   const ref = useRef<HTMLDivElement>(null);
@@ -15,13 +17,13 @@ const View = () => {
 
   const { view, prevState } = usePluginViewContext();
   const [loading, get] = useInstance();
-  //   const action = useCallback(
-  //     (fn: (ctx: Ctx) => void) => {
-  //       if (loading) return;
-  //       get().action(fn);
-  //     },
-  //     [loading],
-  //   );
+  const action = useCallback(
+    (fn: (ctx: Ctx) => void) => {
+      if (loading) return;
+      get().action(fn);
+    },
+    [loading],
+  );
 
   const show = () => {
     console.log('show');
@@ -33,7 +35,9 @@ const View = () => {
     const pos = view.coordsAtPos($anchor.pos);
     ref.current.style.top = `${pos.top + 30}px`;
     ref.current.style.left = `${pos.left - 40}px`;
-    slashProvider.current?.show();
+    setTimeout(() => {
+      slashProvider.current?.show();
+    }, 200);
   };
   const hide = () => {
     console.log('hide');
@@ -68,37 +72,95 @@ const View = () => {
     slashProvider.current?.update(view, prevState);
   });
 
-  //   const command = (e: React.KeyboardEvent | React.MouseEvent) => {
-  //     e.preventDefault(); // Prevent the keyboad key to be inserted in the editor.
-  //     action((ctx) => {
-  //       const view = ctx.get(editorViewCtx);
-  //       const { dispatch, state } = view;
-  //       const { tr, selection } = state;
-  //       const { from } = selection;
+  // const insertTitle1 = (e: React.MouseEvent) => {
+  //   console.log('insertTitle1');
+  //   e.preventDefault();
+
+  //   action((ctx) => {
+  //     const view = ctx.get(editorViewCtx);
+  //     const { dispatch, state } = view;
+  //     const { tr, selection } = state;
+
+  //     const { from } = selection;
+
+  //     if (from === 0) {
+  //       dispatch(tr.deleteRange(0, 0));
+  //     } else {
   //       dispatch(tr.deleteRange(from - 1, from));
-  //       view.focus();
-  //       return callCommand(createCodeBlockCommand.key)(ctx);
-  //     });
-  //   };
+  //     }
+
+  //     view.focus();
+  //     console.log(from);
+  //     return callCommand(createCodeBlockCommand.key)(ctx);
+  //   });
+  // };
+
+  const transformToCode = (e: React.MouseEvent) => {
+    e.preventDefault();
+    action((ctx) => {
+      return callCommand(createCodeBlockCommand.key)(ctx);
+    });
+  };
+
+  const transformToBold = (e: React.MouseEvent) => {
+    e.preventDefault();
+    action((ctx) => {
+      return callCommand(toggleStrongCommand.key)(ctx);
+    });
+  };
+
+  const insertHeading = (level: number) => {
+    action((ctx) => {
+      const view = ctx.get(editorViewCtx);
+      const { dispatch, state } = view;
+      const { tr, selection } = state;
+      const { $from } = selection;
+
+      const safeInsertPos = Math.min($from.end() + 1, tr.doc.content.size);
+      const heading = state.schema.nodes.heading.create({ level: level }, state.schema.text('标题'));
+      tr.insert(safeInsertPos, heading);
+      tr.setSelection(TextSelection.near(tr.doc.resolve(safeInsertPos)));
+      view.dispatch(tr.scrollIntoView());
+      view.focus();
+    });
+  };
 
   return (
-    // <div ref={ref} aria-expanded="false" className="absolute data-[show='false']:hidden">
-    //   <button
-    //     className="text-gray-600 bg-slate-200 px-2 py-1 rounded-lg hover:bg-slate-300 border hover:text-gray-900"
-    //     onKeyDown={(e) => command(e)}
-    //     onMouseDown={(e) => {
-    //       command(e);
-    //     }}
-    //   >
-    //     Code Block
-    //   </button>
-    // </div>
-    <div className="slash-view" ref={ref}>
-      <div className="slash-view-content">
+    <div className="slash-menu-block-view" ref={ref}>
+      <div className="content">
         {/* <div className="slash-view-content-item" onClick={onDelete}>
           删除
         </div> */}
-        <div className="slash-view-content-item">插入标题1</div>
+        <div className="group">
+          <div className="title">在下方插入</div>
+          <div className="items">
+            <div className="item" onClick={() => insertHeading(1)}>
+              H1
+            </div>
+            <div className="item" onClick={() => insertHeading(2)}>
+              H2
+            </div>
+            <div className="item" onClick={() => insertHeading(3)}>
+              H3
+            </div>
+          </div>
+        </div>
+
+        <div className="group">
+          <div className="title">转换为</div>
+          <div className="items">
+            <div className="item">
+              <img src={boldImg} alt="" onClick={transformToBold} />
+            </div>
+            <div className="item">
+              <img src={codeImg} alt="" onClick={transformToCode} />
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="slash-view-content-item" onClick={insertTitle1}>
+          转换为代码
+        </div> */}
       </div>
     </div>
   );
---
+++ packages/milkdown-editor/src/utils/img-helper.ts
@@ -0,0 +1,2 @@
+export { default as codeImg } from '../assets/code.svg';
+export { default as boldImg } from '../assets/bold.svg';
